dd38ab44c8da3373973b9c2ec38b5bee82089550
Peter Hatch <petershatch@gmail.com>
2018-04-16 17:05:14 -0700

    Added Pug support.

--- src/App.js
+++ src/App.js
@@ -4,17 +4,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return pug`
+      div.App
+        header.App-header
+          img.App-logo(src=logo alt="logo")
+          h1.App-title Welcome to React
+        p.App-intro
+          | To get started, edit #[code src/App.js] and save to reload.
+    `;
   }
 }
 
