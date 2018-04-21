b083116d1230a3af69d2f958508d859cbea0ed82
Peter Hatch <petershatch@gmail.com>
2018-04-20 20:12:58 -0700

    Removed remnants of unused design ideas; tweaked styles.

--- src/ItemInputs.js
+++ src/ItemInputs.js
@@ -4,42 +4,43 @@ import './ItemInputs.sass'
 
 function ItemInputs(props) {
   return pug`
-    .edit
-      .have
-        label
-          if props.standardItem
-            input(type="checkbox" checked=true disabled=true)
-          else
+    .center-wrapper
+      .edit
+        .have
+          label
+            if props.standardItem
+              input(type="checkbox" checked=true disabled=true)
+            else
+              input(type="checkbox")
+            svg.have-icon(viewBox="0 0 1 1")
+              use(xlinkHref=(icons + "#checkmark"))
+        .favorite
+          label
             input(type="checkbox")
-          svg.have-icon(viewBox="0 0 1 1")
-            use(xlinkHref=(icons + "#checkmark"))
-      .favorite
-        label
-          input(type="checkbox")
-          svg.favorite-icon(viewBox="0 0 1 1")
-            use(xlinkHref=(icons + "#heart"))
-      fieldset.rating
-        .rating-flex
-          label
-            input(type="radio" name="rating" value="5")
-            svg.rating-icon(viewBox="0 0 1 1")
-              use(xlinkHref=(icons + "#star"))
-          label
-            input(type="radio" name="rating" value="4")
-            svg.rating-icon(viewBox="0 0 1 1")
-              use(xlinkHref=(icons + "#star"))
-          label
-            input(type="radio" name="rating" value="3")
-            svg.rating-icon(viewBox="0 0 1 1")
-              use(xlinkHref=(icons + "#star"))
-          label
-            input(type="radio" name="rating" value="2")
-            svg.rating-icon(viewBox="0 0 1 1")
-              use(xlinkHref=(icons + "#star"))
-          label
-            input(type="radio" name="rating" value="1")
-            svg.rating-icon(viewBox="0 0 1 1")
-              use(xlinkHref=(icons + "#star"))
+            svg.favorite-icon(viewBox="0 0 1 1")
+              use(xlinkHref=(icons + "#heart"))
+        fieldset.rating
+          .rating-flex
+            label
+              input(type="radio" name="rating" value="5")
+              svg.rating-icon(viewBox="0 0 1 1")
+                use(xlinkHref=(icons + "#star"))
+            label
+              input(type="radio" name="rating" value="4")
+              svg.rating-icon(viewBox="0 0 1 1")
+                use(xlinkHref=(icons + "#star"))
+            label
+              input(type="radio" name="rating" value="3")
+              svg.rating-icon(viewBox="0 0 1 1")
+                use(xlinkHref=(icons + "#star"))
+            label
+              input(type="radio" name="rating" value="2")
+              svg.rating-icon(viewBox="0 0 1 1")
+                use(xlinkHref=(icons + "#star"))
+            label
+              input(type="radio" name="rating" value="1")
+              svg.rating-icon(viewBox="0 0 1 1")
+                use(xlinkHref=(icons + "#star"))
   `
 }
 
