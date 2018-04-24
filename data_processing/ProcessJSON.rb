a130050219d4f467c66462a5e433d489eb4caeb9
Peter Hatch <petershatch@gmail.com>
2018-04-23 17:46:52 -0700

    Added achievement info to items.

--- data_processing/ProcessJSON.rb
+++ data_processing/ProcessJSON.rb
@@ -3,8 +3,8 @@ require_relative 'achievements'
 require_relative 'hero_unlocks'
 require_relative 'group_items'
 
-rewards = process_achievements
 items = process_unlocks
+add_achievements_info(items)
 item_groups = group_items(items)
 
 IO.write('../src/data/items.json', JSON.pretty_generate(items))
--- data_processing/achievements.rb
+++ data_processing/achievements.rb
@@ -1,6 +1,32 @@
 require 'json'
 require_relative 'guid'
 
+def add_achievements_info(items)
+  rewards = process_achievements
+
+  rewards.each do |reward_item_id, achievement_description|
+    reward_item = items[reward_item_id]
+    if reward_item['availability'] != 'unknown'
+      raise "Unexpected availability of achievement reward item #{reward_item}"
+    end
+    reward_item['availability'] = 'achievement'
+    reward_item['unlock-condition'] = achievement_description
+
+    {
+      'Lúcioball' => 'summer-games',
+      'Junkenstein' => 'halloween-terror',
+      "Mei's Snowball Offensive" => 'winter-wonderland',
+      'Yeti Hunter' => 'winter-wonderland',
+      'Uprising' => 'archives',
+      'Retribution' => 'archives',
+    }.each do |string, event|
+      if achievement_description.include?(string)
+        reward_item['group'] = event
+      end
+    end
+  end
+end
+
 def process_achievements
   achievements = JSON.load(File.read('input/Achievements.json'))
   raise 'Unexpected format' unless validate_achievements(achievements)
