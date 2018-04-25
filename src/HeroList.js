e046f9812bef62114d1ab3bae6abcb4b8409b362
Peter Hatch <petershatch@gmail.com>
2018-04-24 22:27:44 -0700

    Styled hero select screen.

--- src/HeroList.js
+++ src/HeroList.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import groupData from './data/groups.json'
+import './HeroList.sass'
 
 function HeroList() {
   let heroNames = Object.keys(groupData.heroes)
   return pug`
+    h1 Heroes
     nav.heroes
       each name in heroNames
-        div(key=name)
-          Link(to='/hero/' + name)
-            = name
+        Link(key=name to='/hero/' + name)
+          img(src='/hero-select/' + name.replace(':', '') + '.png')
+          if name === 'Widowmaker'
+            span.small
+              = name
+          else
+            span
+              = name
   `
 }
 
